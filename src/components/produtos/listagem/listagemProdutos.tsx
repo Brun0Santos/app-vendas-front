262465d1b59d54b5d9541d24fb0a37122b107740
Bruno Santos <bruno_st17@hotmail.com>
2023-08-10 09:41:08 -0300

    feat: configurando a listagem de produtos

--- src/components/produtos/listagem/listagemProdutos.tsx
+++ src/components/produtos/listagem/listagemProdutos.tsx
@@ -1,15 +1,45 @@
-import { Button } from '@chakra-ui/react';
+import { Box, Button, Flex } from '@chakra-ui/react';
 import Link from 'next/link';
 
+// import { MdPlaylistAdd } from 'react-icons/md';
+import { Produto } from '@/app/models/produtos/produtosModel';
 import Layout from '@/components/layout/layout';
+
+import { TabelaProdutos } from './tabela/tabelaProdutos';
+
+const produtosArrays: Array<Produto> = [
+  {
+    id: '1',
+    nome: 'teste',
+    codProduto: '12121',
+    dataCadastro: '',
+    descricao: '',
+    preco: 12121,
+  },
+  {
+    id: '2',
+    nome: 'teste',
+    codProduto: '12121',
+    dataCadastro: '',
+    descricao: '',
+    preco: 12121,
+  },
+];
 
 export function ListagemProdutos() {
   return (
     <Layout titulo="Produtos">
-      <Link href={'/cadastro/produtos'}>
-        <Button colorScheme="blue">Novo Produto</Button>
-      </Link>
-      <br />
+      <Box w={'100%'}>
+        <Link href={'/cadastro/produtos'}>
+          <Button bg="green.200" m={'30px 0 15px 20px'} _hover={{ bg: 'gray.400' }}>
+            Novo Produto
+          </Button>
+        </Link>
+
+        <Flex flexDirection={'column'}>
+          <TabelaProdutos produtos={produtosArrays} />
+        </Flex>
+      </Box>
     </Layout>
   );
 }
---
+++ src/components/produtos/listagem/tabela/tabelaProdutos.tsx
@@ -0,0 +1,60 @@
+import { Button, Table, TableContainer, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react';
+import { BsTrash } from 'react-icons/bs';
+import { LuEdit } from 'react-icons/lu';
+
+import { Produto } from '@/app/models/produtos/produtosModel';
+
+interface ProdutoRowProps {
+  children?: React.ReactNode;
+  produto: Produto;
+}
+
+interface ListaProdutos {
+  children?: React.ReactNode;
+  produtos: Array<Produto>;
+}
+
+export function TabelaProdutos({ produtos }: ListaProdutos) {
+  return (
+    <div>
+      <Table variant="simple" size={'lg'}>
+        <Thead>
+          <Tr>
+            <Th>Id</Th>
+            <Th>Código do Produto</Th>
+            <Th>Nome</Th>
+            <Th isNumeric>Preço</Th>
+            <Th></Th>
+            <Th></Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {produtos.map((p, i) => (
+            <ProdutoRow produto={p} key={i} />
+          ))}
+        </Tbody>
+      </Table>
+    </div>
+  );
+}
+
+const ProdutoRow = ({ produto }: ProdutoRowProps) => {
+  return (
+    <Tr key={produto.id} _hover={{ bg: 'gray.50' }}>
+      <Td>{produto.id}</Td>
+      <Td>{produto.codProduto}</Td>
+      <Td>{produto.nome}</Td>
+      <Td isNumeric>{produto.preco}</Td>
+      <Td fontSize={'20px'} textAlign={'end'}>
+        <button>
+          <LuEdit />
+        </button>
+      </Td>
+      <Td fontSize={'20px'}>
+        <button>
+          <BsTrash />
+        </button>
+      </Td>
+    </Tr>
+  );
+};
